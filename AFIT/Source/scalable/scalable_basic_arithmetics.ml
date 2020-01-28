352c6700e1a0db502db43a149018f60ef9c7bc3c
Johanna Levasseur <johanna.levasseur@epita.fr>
2020-01-29 00:38:00 +0100

    Je commence basics arithmetic de scalabe

--- AFIT/Source/scalable/scalable_basic_arithmetics.ml
+++ AFIT/Source/scalable/scalable_basic_arithmetics.ml
@@ -6,7 +6,33 @@ open Scalable
     @param bA non-zero bitarray.
     @param bB non-zero bitarray.
 *)
+
+let rec power a = function
+0 -> 1
+  | i -> a*(power a (i-1));;
+
+let to_int bA =
+  let (sign, rest) = match bA with
+      [] -> (0,[])
+    | 0::s -> (1,s)
+    | _::s -> ((-1),s) in
+  let rec to_int_r array deg = match array with
+      [] -> 0
+    | e::s -> e*(power 2 deg) + (to_int_r s (deg+1))
+  in sign*(to_int_r rest 0);;
+
+let rec gcd a b = match (a,b) with
+    (a,b) when a < 0 -> gcd (-a) b
+  | (a,b) when b < 0 -> gcd a (-b)
+  | _ -> let rec gdc_rec a b = match (div a b) with
+      (a,0) -> gcd_rec b d
+	 in gcd_rec a b;;
+
 let gcd_b bA bB = []
+  bA = to_int bA
+  bB = to_int bB
+  x = gcd bA bB
+  
 
 (** Extended euclidean division of two integers NOT OCAML DEFAULT.
     Given non-zero entries a b computes triple (u, v, d) such that
